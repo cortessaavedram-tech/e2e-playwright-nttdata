03e1da85a828aea9a9018b5fe7c75f642d812588
MªÁngeles Cortés Saavedra <cortessaavedram@gmail.com>
2026-01-12 11:31:36 +0100

    Update test_form.py

--- tests/test_form.py
+++ tests/test_form.py
@@ -2,6 +2,7 @@ from playwright.sync_api import Page, expect
 import re
 
 def test_contact_form(page: Page):
+    #Scenario: Fill the contact form with an empty mandatory field
     print("Given the user is on the Contact page")
     page.goto("https://es.nttdata.com/contact-us")
     print("When the user leaves the 'Name' field empty")
@@ -9,7 +10,17 @@ def test_contact_form(page: Page):
     page.get_by_role("textbox", name="Nombre*").clear()
     #Leave the field empty
     page.get_by_role("textbox", name="Nombre*").fill("")
-    #print("Then Campo requerido message should appear")
+    print("Then error message should appear")
     expect(page.get_by_text("Campo requerido")).to_be_visible()
+
+    #Scenario: Fill the Telephone field with invalid data
+    print("Given the user is on the Contact page")
+    page.goto("https://es.nttdata.com/contact-us")
+    print("When te user fills the 'Telephone' field with invalid data")
+    page.get_by_role("textbox", name="Teléfono").fill("invalid_phone")
+    print("Then eror message should appear indicating invalid phone number")
+    expect(page.get_by_text("Formato de número de teléfono incorrecto. Por favor, sigue el formato de ejemplo: +34 12 345 6789")).to_be_visible()
+
+
 
     
