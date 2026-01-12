53727bf0498152cb5f755084f6115a7703aedc07
MªÁngeles Cortés Saavedra <cortessaavedram@gmail.com>
2026-01-12 11:40:43 +0100

    Update test_form.py-teléfono

--- tests/test_form.py
+++ tests/test_form.py
@@ -16,8 +16,10 @@ def test_contact_form(page: Page):
     #Scenario: Fill the Telephone field with invalid data
     print("Given the user is on the Contact page")
     page.goto("https://es.nttdata.com/contact-us")
+    #Clear the placeholder text
+    page.get_by_role("textbox", name="Teléfono*").clear()
     print("When te user fills the 'Telephone' field with invalid data")
-    page.get_by_role("textbox", name="Teléfono").fill("invalid_phone")
+    page.get_by_role("textbox", name="Teléfono*").fill("invalid_phone")
     print("Then eror message should appear indicating invalid phone number")
     expect(page.get_by_text("Formato de número de teléfono incorrecto. Por favor, sigue el formato de ejemplo: +34 12 345 6789")).to_be_visible()
 
