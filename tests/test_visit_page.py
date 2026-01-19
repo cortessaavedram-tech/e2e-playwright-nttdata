6152fb89315fcdce2740c83337b31c460aca633c
MªÁngeles Cortés Saavedra <cortessaavedram@gmail.com>
2026-01-19 10:43:12 +0100

    Img add to Visit_page

--- tests/test_visit_page.py
+++ tests/test_visit_page.py
@@ -4,4 +4,11 @@ def test_visit(page: Page):
     print("Given user visit homepage")
     page.goto("https://es.nttdata.com/")
     
+def test_images(page: Page):
+    #Images of home page are visible
+    print("Given the user is on the Home page")
+    page.goto("https://es.nttdata.com/")
+    print("When the user visit the page")
+    print("Then the image should be visible")
+    expect(page.get_by_role("img", name="NTT-DATA-servizi-e-consulenza-imprese")).to_be_visible()
 
