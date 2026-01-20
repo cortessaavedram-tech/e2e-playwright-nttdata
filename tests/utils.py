9828719ee37a04f8193f94ec9a48e71fcdf2d066
MªÁngeles Cortés Saavedra <cortessaavedram@gmail.com>
2026-01-20 12:10:47 +0100

    Update utils_cookies_mobile

--- tests/utils.py
+++ tests/utils.py
@@ -11,6 +11,11 @@ def is_mobile(page: Page):
     return is_mobile
 
 def accept_cookies(page):
-    btn_cookies = page.get_by_role("button", name=re.compile(r"Aceptar", re.I))
-    if btn_cookies.is_visible():
-        btn_cookies.click()
+    if is_mobile(page):
+        btn_cookies = page.get_by_role("button", name=re.compile(r"Okay", re.I))
+        if btn_cookies.is_visible():
+            btn_cookies.click()
+    else:
+        btn_cookies = page.get_by_role("button", name=re.compile(r"Aceptar", re.I))
+        if btn_cookies.is_visible():
+            btn_cookies.click()
