53586a3f2f8eee60d09a88a4dfa5d648b39a6398
MªÁngeles Cortés Saavedra <cortessaavedram@gmail.com>
2026-01-20 12:28:40 +0100

    Cookies utils

--- tests/utils.py
+++ tests/utils.py
@@ -15,6 +15,7 @@ def accept_cookies(page):
         btn_cookies = page.get_by_role("button", name=re.compile(r"Okay", re.I))
         if btn_cookies.is_visible():
             btn_cookies.click()
+        
     else:
         btn_cookies = page.get_by_role("button", name=re.compile(r"Aceptar", re.I))
         if btn_cookies.is_visible():
