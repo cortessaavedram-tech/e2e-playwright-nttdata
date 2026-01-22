d487c279b09e741f1ce050c08cfdb58cdcf5725d
MªÁngeles Cortés Saavedra <cortessaavedram@gmail.com>
2026-01-22 12:33:43 +0100

    Delete test_visual.py

--- tests/test_visual_homepage.py
+++
@@ -1,7 +0,0 @@
-from playwright.sync_api import Page, expect
-
-def test_visual_homepage(page: Page, assert_snapshot):
-    print("Given user visit homepage")
-    page.goto("https://es.nttdata.com/")
-    
-    assert_snapshot(page.screenshot())
